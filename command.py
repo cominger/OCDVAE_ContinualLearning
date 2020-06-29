5a05f86aa2502441d96d7ffc0959fa3e0d17ea16
cominger <cominger10@gmail.com>
2020-06-30 00:55:03 +0900

    update command

--- command.py
+++ command.py
@@ -15,3 +15,8 @@ CUDA_VISIBLE_DEVICES=0 python main.py --dataset Flower17 -j 6 -p 64 -b 128 -a WR
 CUDA_VISIBLE_DEVICES=1 python main.py --dataset Flower17 -j 3 -p 64 -b 128 -a WRN_gan --wrn-depth 26 --gan --visualization-epoch 50 --incremental-data True --num-base-tasks 1 --num-increment-tasks 1 --num-dis-feature 64 --openset-generative-replay True -genreplay True --epoch 2000 --var-latent-dim 64  --blur --FID
 # DGR test
 CUDA_VISIBLE_DEVICES=0 python main.py --dataset Flower17 -j 3 -p 64 -b 32 -a WRN_enc_res_dec_gan --wrn-depth 26 --gan --visualization-epoch 20 --incremental-data True --num-base-tasks 1 --num-increment-tasks 1 --num-dis-feature 64 --openset-generative-replay True -genreplay True --epoch 2000 --var-latent-dim 64  --l1-weight 1 --var-gan-weight 1 --FID --gan-loss DGR -d
+
+
+CUDA_VISIBLE_DEVICES=1 python main.py --dataset CIFAR10 -j 2 -p 32 -b 128 -a WRN_gan --wrn-depth 26 --gan --visualization-epoch 20 --incremental-data True --num-base-task 1 --num-increment-tasks 1 --num-dis-feature 64 --openset-generative-replay True -genreplay True --epoch 2000 --var-latent-dim 128  --l1-weight 1 --var-gan-weight 1 --var-beta 0.1 --FID --gan-loss hinge-gan --learning-rate 0.0002 --gen-learning-rate 0.0002 --dis-learning-rate 0.0002
+
+CUDA_VISIBLE_DEVICES=0 python main.py --dataset Flower17 -j 4 -p 64 -b 32 -a WRN_gan --wrn-depth 26 --gan --visualization-epoch 20 --incremental-data True --num-base-task 1 --num-increment-tasks 1 --num-dis-feature 64 --openset-generative-replay True -genreplay True --epoch 2000 --var-latent-dim 128  --l1-weight 1 --var-gan-weight 0.025 --var-beta 0.1 --FID --gan-loss hinge-gan --learning-rate 0.0001 --gen-learning-rate 0.0001 --dis-learning-rate 0.0001
---
+++ lib/Models/generators/resnet32.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn import init
+
+# from models.generators.resblocks import Block
+from .resblocks import Block
+
+
+class ResNetGenerator(nn.Module):
+    """Generator generates 64x64."""
+
+    def __init__(self, num_features=64, bottom_width=4,
+                 activation=F.relu, num_classes=0):
+        super(ResNetGenerator, self).__init__()
+        self.num_features = int(num_features/10)
+        self.bottom_width = bottom_width
+        self.activation = activation
+        self.num_classes = num_classes
+
+        self.block2 = Block(num_features, self.num_features * 4,
+                            activation=activation, upsample=True, 
+                            num_classes=num_classes) 
+        self.block3 = Block(self.num_features * 4, self.num_features * 2,
+                            activation=activation, upsample=True,
+                            num_classes=num_classes)
+        self.block4 = Block(self.num_features * 2, self.num_features *2,
+                            activation=activation, upsample=False,
+                            num_classes=num_classes)
+        self.block5 = nn.Conv2d(self.num_features*2, self.num_features, 1, 1)
+        self.b6 = nn.BatchNorm2d(self.num_features)
+        self.conv6 = nn.Conv2d(self.num_features, 3, 1, 1)
+        
+        # self._initialize()
+
+    # def _initialize(self):
+        # init.xavier_uniform_(self.l1.weight.tensor)
+        # init.xavier_uniform_(self.conv7.weight.tensor)
+
+    def forward(self, z, y=None, **kwargs):
+        # h = self.l1(z).view(z.size(0), -1, self.bottom_width, self.bottom_width)
+        # h = z.view(z.size(0), self.num_features*16, self.bottom_width, self.bottom_width)
+        h = z.view(z.size(0), -1, self.bottom_width, self.bottom_width)
+        for i in range(2, 5):
+            h = getattr(self, 'block{}'.format(i))(h, y, **kwargs)
+        # h = self.activation(self.b6(h))
+        h = self.block5(h)
+        h = self.b6(h)
+        # return torch.tanh(self.conv6(h))
+        return self.conv6(h)
